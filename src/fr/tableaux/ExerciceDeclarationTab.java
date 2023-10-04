71f2c48c8b50d0d49caaa88fde88505f4e1c50c4
jbarn9 <jbarenne@laposte.net>
2023-10-04 16:33:58 +0200

    tableaux2

--- src/fr/tableaux/ExerciceDeclarationTab.java
+++ src/fr/tableaux/ExerciceDeclarationTab.java
@@ -7,6 +7,7 @@ public class ExerciceDeclarationTab {
         int[] tab = {8, -7, 12, 1,-2,14,17,9};
         System.out.println(tab[0]);
         System.out.println(tab.length);
+        System.out.println(tab[tab.length-1]);
         System.out.println(tab[2]);
         System.out.println(tab[10]); //out of order
         
