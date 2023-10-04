1ccf4c3b8568bbebd1bbfde16556c8293aa181ab
jbarn9 <jbarenne@laposte.net>
2023-10-04 17:43:46 +0200

    tp 1

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -2,6 +2,18 @@ package fr.declaration.variable;
 
 public class DeclarationApp {
 	 public static void main(String[] args) {
-		 
+		 byte b =10;
+		 short s=-17282;
+		 int i =2846352;
+		 long l =1000L;
+		 float f =2.5F;
+		 double d=6.4D;
+		 char c ='c';
+		 boolean myBool = true;
+		 System.out.println(c);
+		 String randomString = "Voici le résultat d'un calcul : 1+5=6";
+		 System.out.println(randomString);
+		 randomString = "Voici le résultat d'un calcul :\n 1+5=6";
+		 System.out.println(randomString);
 	 }
 }
--- src/fr/tableaux/ExerciceTableauChaines.java
+++ src/fr/tableaux/ExerciceTableauChaines.java
@@ -7,10 +7,16 @@ public class ExerciceTableauChaines {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        String tabCities[] = {"Montpellier", "Paris", "Bordeaux", "Lyon", "Limoge"};
-       System.out.println(tabCities.length);
+       System.out.println("/");
        for (int i = 0; i < tabCities.length; i++) {
     	    System.out.println(tabCities[i]);
     	}
+       System.out.println("/");
+       for (String nom : tabCities) {
+   	    System.out.println(nom);
+      	} 
+       System.out.println("/");
+       System.out.println(tabCities.length);
        tabCities[3]="Reims";
        System.out.println(tabCities[3]);
        for (int i = 0; i < tabCities.length; i++) {
