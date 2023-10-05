73574b0a41206d21f287d85ba226ad2f9d7d7736
jbarn9 <jbarenne@laposte.net>
2023-10-05 17:30:06 +0200

    Approche imp exo interactifSommeArith

--- src/fr/algorithme/InteractifSommeArithmetique.java
+++ src/fr/algorithme/InteractifSommeArithmetique.java
@@ -1,10 +1,18 @@
 package fr.algorithme;
 
+import java.util.Scanner;
+
 public class InteractifSommeArithmetique {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int nb = scanner.nextInt();
+		int nbFinal = 0;
+		for (int i = 1; i <= nb; i++) {
+			nbFinal += i; 
+			System.out.println(nbFinal);
+		}
 	}
 
 }
